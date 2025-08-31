1fde131a0196fcfac7c5066fdc604c527ba87652
Stuti Porwal <porwalstuti01@gmail.com>
2025-08-31 19:59:05 +0530

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,10 +2,6 @@
 #include<queue>
 using namespace std;
 
-
-// Insert a Node in BST
-// Insert a Node in BST - T.C. - O(log n) - homework - derivation 
-
 class Node {
     public:
     int data;
@@ -142,17 +138,16 @@ int main() {
     cout << " max value is " << maxValue(root) -> data << endl;
 
     root = del(root, 30); // deletion
-
+    
     cout<<"printing the BST" << endl;
-
     levelOrderTraversal(root);
-
+    
     cout << "Printing Inorder" << endl;
     inorder(root);
-
+    
     cout <<  endl << "Printing Preorder" << endl;
     preorder(root);
-
+    
     cout << endl << "Printing Postorder" << endl;
     postorder(root);
 
@@ -160,101 +155,4 @@ int main() {
     cout << " max value is " << maxValue(root) -> data << endl;
 
     return 0;
-}
-
-// CodeStudio
-// Search a number in BST (Binary Searh Tree), if the number is present in BST or not.
-// t.c. O(n) or O(h) - height homework - search t.c. and s.c.
-
-bool searchInBST(Node* root, int x) {
-    // base case
-    if(root == NULL) {
-        return false;
-    }
-
-    if(root -> data == x)
-        return true;
-
-    if(root -> data > x){
-        // left part me
-        return (root -> left, x);
-    }
-
-    else {
-        //right part me
-        return (root -> right, x);
-    }
-}
-
-// find min / max in bst
-
-Node* minValue(Node* root) {
-    Node* temp = root;
-
-    while(temp -> left != NULL) {
-        temp = temp -> left;
-    }
-
-    return temp;
-}
-
-Node* maxValue(Node* root) {
-    Node* temp = root;
-
-    while(temp -> right != NULL) {
-        temp = temp -> right;
-    }
-
-    return temp;
-}
-
-// delete a node in BST
-
-Node* del(Node*  root, int val){
-    // base case
-    if(root == NULL){
-        return root;
-    }
-
-    if(root -> data == val) {
-
-        if(root -> left == NULL && root -> right == NULL) {
-            delete root;
-            return NULL;
-        }
-
-        // left child
-        if(root -> left != NULL && root -> right == NULL) {
-            Node* temp = root -> left;
-            delete root;
-            return temp;
-        }
-
-        // right child
-        if(root -> left == NULL && root -> right != NULL) {
-            Node* temp = root -> right;
-            delete root;
-            return temp;
-        }
-
-        if(root -> left != NULL && root -> right != NULL){
-            int mini = minValue(root -> right) -> data;
-            root -> data = mini;
-            root -> right = del(root -> right, mini);
-            return root;
-        }
-
-    }
-
-    else if (root -> data > val) {
-        //left part me gye
-        root -> left = del(root ->left, val);
-        return root;
-    }
-
-    else{
-        // right part me gye
-        root -> right = del(root ->right, val);
-        return root;
-    }
 }
