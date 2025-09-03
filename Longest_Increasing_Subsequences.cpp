949d0b4e094c35876eb8d731a012e998999164be
Stuti Porwal <porwalstuti01@gmail.com>
2025-09-03 16:53:18 +0530

    Add files via upload

---
+++ Longest_Increasing_Subsequences.cpp
@@ -0,0 +1,53 @@
+/* Longest Increasing Subsequence problem using dynamic programming
+ Time Complexity: O(n^2)
+ Space Complexity: O(n)
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int maxLength = 1;
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
+            }
+            maxLength = max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    
+    // Example 1
+    vector<int> nums1 = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << "Longest Increasing Subsequence length: " << sol.lengthOfLIS(nums1) << endl; 
+
+    // Example 2
+    vector<int> nums2 = {0, 1, 0, 3, 2, 3};
+    cout << "Longest Increasing Subsequence length: " << sol.lengthOfLIS(nums2) << endl; 
+
+    // Example 3
+    vector<int> nums3 = {7, 7, 7, 7, 7, 7, 7};
+    cout << "Longest Increasing Subsequence length: " << sol.lengthOfLIS(nums3) << endl; 
+    
+    return 0;
+}
