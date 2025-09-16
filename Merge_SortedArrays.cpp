6f016fb8812b9c3f3c47f6db8fff4954556e9d62
Stuti Porwal <porwalstuti01@gmail.com>
2025-09-16 22:55:04 +0530

    Update Merge_SortedArrays.cpp

--- Merge_SortedArrays.cpp
+++ Merge_SortedArrays.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 using namespace std;
 
-// Definition for singly-linked list.
 struct ListNode {
     int val;
     ListNode *next;
@@ -53,14 +52,11 @@ void printList(ListNode* head) {
     cout << "nullptr" << endl;
 }
 
-// --- Example Usage ---
 int main() {
-    // Create list 1: 1 -> 2 -> 4
     ListNode* list1 = new ListNode(1);
     list1->next = new ListNode(2);
     list1->next->next = new ListNode(4);
     
-    // Create list 2: 1 -> 3 -> 4
     ListNode* list2 = new ListNode(1);
     list2->next = new ListNode(3);
     list2->next->next = new ListNode(4);
