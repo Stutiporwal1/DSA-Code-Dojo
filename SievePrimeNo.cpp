694040b0bf85bae55f6f14326c92feec10c7e413
Stuti Porwal <porwalstuti01@gmail.com>
2025-09-17 20:24:24 +0530

    Update SievePrimeNo.cpp

--- SievePrimeNo.cpp
+++ SievePrimeNo.cpp
@@ -1,22 +1,29 @@
 // Solving leetcode problem no. 204 - count primes (Give an integer n, return the no. of prime no.s that are strictly less than n) using Sieve of Eratosthenes 
-#include<iostream>
-#include<vector>
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
-    public:
+public:
     int countPrimes(int n) {
-        int cnt = 0;
-        vector<bool> prime(n+1, true);
-        prime[0] = prime[1] = false;
-        for(int i =2; i < n; i++) {
-            if(prime[i]) {
-                cnt++;
-                for(int j = 2*i; j<n; j = j+i){
-                    prime[j] = 0;
+        if (n <= 2) return 0; // No primes less than 2
+        
+        vector<bool> prime(n, true); // only need size n
+        prime[0] = prime[1] = false; // 0 and 1 are not prime
+        
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
                 }
             }
         }
+        
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) cnt++;
+        }
         return cnt;
     }
-}; 
+};
