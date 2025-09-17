b32df50cb7a4c1ccf89b291a6f2d17757fb8ad8f
Stuti Porwal <porwalstuti01@gmail.com>
2025-09-17 20:22:31 +0530

    Update Smallest_Range_from_K_SortedList.cpp

--- Smallest_Range_from_K_SortedList.cpp
+++ Smallest_Range_from_K_SortedList.cpp
@@ -3,48 +3,55 @@
  https://www.codingninjas.com/codestudio/problems/smallest-range-from
 */
 
-#include<iostream>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <climits>
 using namespace std;
 
 class compare {
 public:
-    bool operator()(pair<int, int> a, pair<int, int> b) {
-        return a.first > b.first; // Min-heap based on the first element
+    bool operator()(tuple<int,int,int> a, tuple<int,int,int> b) {
+        return get<0>(a) > get<0>(b); // min-heap based on element value
     }
 };
-int kSorted(vector<vector<int>>& kSortedArray) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, compare> minHeap;
-    int maxElement = INT_MIN; // To track the maximum element in the current range
-    int rangeStart = 0, rangeEnd = 0; // To store the smallest range
-    int minRange = INT_MAX; // To store the minimum range found
 
-    // Initialize the min-heap with the first element of each sorted list
+pair<int,int> kSorted(vector<vector<int>>& kSortedArray) {
+    priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, compare> minHeap;
+    int maxElement = INT_MIN; 
+    int rangeStart = 0, rangeEnd = 0; 
+    int minRange = INT_MAX; 
+
+    // Initialize heap with first element of each list
     for (int i = 0; i < kSortedArray.size(); i++) {
         if (!kSortedArray[i].empty()) {
-            minHeap.push({kSortedArray[i][0], i});
+            minHeap.push({kSortedArray[i][0], i, 0}); // {value, listIndex, elementIndex}
             maxElement = max(maxElement, kSortedArray[i][0]);
         }
     }
+
     while (minHeap.size() == kSortedArray.size()) {
-        auto [minElement, listIndex] = minHeap.top();
+        auto [minElement, listIndex, eleIndex] = minHeap.top();
         minHeap.pop();
 
-        // Update the range if it's smaller than the previous minimum range
+        // Update best range
         if (maxElement - minElement < minRange) {
             minRange = maxElement - minElement;
             rangeStart = minElement;
             rangeEnd = maxElement;
         }
-        // Move to the next element in the same list
-        if (++listIndex < kSortedArray[listIndex].size()) {
-            minHeap.push({kSortedArray[listIndex][listIndex], listIndex});
-            maxElement = max(maxElement, kSortedArray[listIndex][listIndex]);
+
+        // Move to next element in the same list
+        if (eleIndex + 1 < kSortedArray[listIndex].size()) {
+            int nextVal = kSortedArray[listIndex][eleIndex + 1];
+            minHeap.push({nextVal, listIndex, eleIndex + 1});
+            maxElement = max(maxElement, nextVal);
         }
     }
-    return minRange;
+
+    return {rangeStart, rangeEnd};
 }
+
 int main() {
     int k;
     cout << "Enter the number of sorted lists: ";
@@ -61,7 +68,9 @@ int main() {
             cin >> kSortedArray[i][j];
         }
     }
-    int result = kSorted(kSortedArray);
-    cout << "The smallest range is: " << result << endl;
+
+    auto result = kSorted(kSortedArray);
+    cout << "The smallest range is: [" << result.first << ", " << result.second << "]" << endl;
     return 0;
-} 
+}
+
