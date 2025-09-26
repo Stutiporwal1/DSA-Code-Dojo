028134334f82d88959a03af627c582736ff9bf26
Stuti Porwal <porwalstuti01@gmail.com>
2025-09-26 20:05:50 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -6,16 +6,20 @@ int main() {
   cin >> n;
 
   int sum = 0;
+  int original = n;
   while(n != 0) {
   int ld = n % 10;
   sum = (sum * 10) + ld;
   n = n / 10;
   }
 
-  if (sum == n){
+  if (sum == original){
     cout << "Palindrome Number" << endl;
   }
   else {
   cout << "Not a Palindrome" << endl;
   }
+
+  return 0;
+  
 }
