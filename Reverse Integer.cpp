af9fd0eb5e674fc6b7c277169b9fba43ec4e8940
Stuti Porwal <porwalstuti01@gmail.com>
2025-09-26 20:21:20 +0530

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,1 +1,24 @@
+// Reverse an Integer
 
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+
+  int duplicate = n;
+  int ans = 0;
+  while(n != 0) {
+    int ld = n % 10;
+    ans = (ans * 10) + ld;
+    n = n / 10;
+  }
+
+  if(duplicate == ans){
+    return ans;
+  }
+  else {
+    return -1;
+  }
+}
